3ee9f18c7978da174d36377bf544cd0c1cd1213f
Omar <oac.mail@gmail.com>
2024-05-17 16:57:34 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Docker React Python Project
+
+A simple project with a React.js frontend and a Python backend using Docker.
+
+## How to Run
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/YOUR_GITHUB_USERNAME/docker-react-python.git
+   cd docker-react-python
+   ```
+
+2. Build and run the Docker containers:
+   ```bash
+   docker-compose up -d
+   ```
+
+3. Access the React app at `http://<Raspberry Pi IP>:5000` and the Python API at `http://<Raspberry Pi IP>:5001`.
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        fetch('http://<Raspberry Pi IP>:5001/api/hello')
+            .then(response => response.json())
+            .then(data => setMessage(data.message));
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>{message}</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
